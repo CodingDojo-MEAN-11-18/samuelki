cc0febaa8cd9562862e10e87ed81bf0a696f44f5
unknown <samuelki62@gmail.com>
2018-11-21 00:52:37 -0800

    platform notes

--- 03-express/00-platform/Hello_Express/server.js
+++ 03-express/00-platform/Hello_Express/server.js
@@ -5,7 +5,7 @@ console.log("Let's find out what express is", express);
 var app = express();
 console.log("Let's find out what app is", app);
 // use app's get method and pass it the base route '/' and a callback
-app.get('/', function(request, response) {
+app.get('/', function (request, response) {
     // just for fun, take a look at the request and response objects
    console.log("The request object", request);
    console.log("The response object", response);
@@ -15,4 +15,24 @@ app.get('/', function(request, response) {
 // tell the express app to listen on port 8000, always put this at the end of your server.js file
 app.listen(8000, function() {
   console.log("listening on port 8000");
+})
+
+
+// this is the line that tells our server to use the "/static" folder for static content
+app.use(express.static(__dirname + "/static"));
+
+
+// this sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get("/users", function (request, response) {
+  // hard-coded user data
+  var users_array = [
+    {name: "Michael", email: "Michael@codingdojo.com"},
+    {name: "Jay", email: "Jay@codingdojo.com"},
+    {name: "Brendan", email: "Brendan@codingdojo.com"},
+    {name: "Andrew", email: "Andrew@codingdojo.com"}
+  ];
+  response.render('users', {users: users_array});
 })
