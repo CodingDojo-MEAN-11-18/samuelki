6d848e367f98e4a112eec6c1038089a19b744617
unknown <samuelki62@gmail.com>
2018-11-26 06:05:32 -0800

    Survey Form Revisited fixes

--- 03-express/02-socket.io/01-survey-form-revisited/server.js
+++ 03-express/02-socket.io/01-survey-form-revisited/server.js
@@ -5,7 +5,6 @@ const parser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 8000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
-const io = require('socket.io')(server);
 
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(parser.urlencoded({extended: true}));
@@ -16,17 +15,22 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+// this gets the socket.io module
+const io = require('socket.io')(server);
+
+// listen to connection even from the client side
 io.on('connection', function (socket) {
+  // server listens to "posting_form" event
   socket.on('posting_form', function (data) {
     let random_number = Math.floor((Math.random()*1000) + 1);
+    // let message = `You emitted the following information to the server:
+    // Name: ${data.name},
+    // Dojo Location: ${data.location},
+    // Favorite Language: ${data.language},
+    // Comment: ${data.comment}`;
 
-    let message = `You emitted the following information to the server:
-    Name: ${data.name},
-    Dojo Location: ${data.location},
-    Favorite Language: ${data.language},
-    Comment: ${data.comment}`;
-  
-    socket.emit('updated_message', {response: message});
+    // will emit the data to the client
+    socket.emit('updated_message', {response: data});
     socket.emit('random_number', {response: random_number});
-  });
+  })
 });
