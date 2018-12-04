831d6a8ba37446ffd9737b87a06ca698cd4d6469
unknown <samuelki62@gmail.com>
2018-12-04 01:47:06 -0800

    added Hello Angular

--- 04-mongodb/05-api/02-restful-task-api/server.js
+++ 04-mongodb/05-api/02-restful-task-api/server.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.use(parser.json());
+app.use(express.static(__dirname + '/public/dist/public'));
 
 require('./server/config/database');
 require('./server/config/routes')(app);
