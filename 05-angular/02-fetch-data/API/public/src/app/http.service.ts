8af2c65cde80109f1c15dcaa08519a235e5169bb
unknown <samuelki62@gmail.com>
2018-12-06 15:48:17 -0800

    added Pokemon API assignment

--- 05-angular/02-fetch-data/API/public/src/app/http.service.ts
+++ 05-angular/02-fetch-data/API/public/src/app/http.service.ts
@@ -8,15 +8,24 @@ export class HttpService {
 
   constructor(private _http: HttpClient) {
     this.getPokemon();
+    this.getAbility();
   }
 
   getPokemon() {
     let bulbasaur = this._http.get<PokemonData>('https://pokeapi.co/api/v2/pokemon/1/');
     bulbasaur.subscribe(data => {
       console.log("Got Bulbasaur!", data);
+      console.log("Bulbasaur's abilities are:")
       for (const ability of data.abilities) {
         console.log(ability.ability.name);
       }
+    });
+  }
+
+  getAbility() {
+    let ability = this._http.get('https://pokeapi.co/api/v2/ability/34/');
+    ability.subscribe((data: any) => {
+      console.log(`${data.pokemon.length - 1} other pokemon have the chlorophyll ability`)
     });
   }
 }
